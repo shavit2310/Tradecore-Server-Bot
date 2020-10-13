7975084ba660f7e94ff279abfefcc5abcaf64e4d
Social network -Django-Python <sg.sapir@gmail.com>
2020-10-13 08:02:47 +0300

    Update models.py
    reorgnize func

--- Server/core/models.py
+++ Server/core/models.py
@@ -6,17 +6,15 @@ from django.db import models
 from pyhunter import PyHunter
 import logging
 
-my_hunter_api_key = '''NEED TO ADD ON CLONE'''
-clearbit.key = '''NEED TO ADD ON CLONE'''
+# my_hunter_api_key = '''NEED TO ADD ON CLONE'''
+# clearbit.key = '''NEED TO ADD ON CLONE'''
 
 
 class MISMATCH_BETWEEN_ENTITIES(Exception):
-    # print
     pass
 
 
 class NOT_UNIQUE(Exception):
-    # print
     pass
 
 
@@ -25,7 +23,6 @@ class NOT_UNIQUE(Exception):
 def _mail_verification(mail):
     '''Mail validation via hunter site'''
     try:
-
         hunter = PyHunter(my_hunter_api_key)
         user_data = hunter.email_verifier(mail)
         # On riski & deliverable continue
@@ -66,70 +63,6 @@ def _decode(verif_code):
         return {e, -1}
 
 
-def _login(verif_code):
-    '''Verify known user'''
-    try:
-        op_mail, op_id = _decode(verif_code)
-        if op_id == -1:
-            raise Exception('JWT code not decrypted')
-
-        user_obj = User.objects.get(pk=op_id)
-        if user_obj.pk != None:
-            if user_obj.email == op_mail:
-                return user_obj.id
-        else:
-            raise Exception('Verification failed')
-
-    except Exception as e:
-        return {e}
-
-
-def _retreive_user(user_id):
-    ''' Uploading user object '''
-    try:
-        user_obj = User.objects.get(pk=user_id)
-        if not user_obj:
-            raise Exception('User object is not found')
-
-        return user_obj
-
-    except Exception as e:
-        logging.warning(e)
-        return {e}
-
-
-def _is_post(post_id):
-    ''' Uploading post object '''
-    try:
-        post_obj = Post.objects.get(pk=post_id)
-        if not post_obj:
-            raise Exception('Post object is Not found')
-
-        return post_obj
-
-    except Exception as e:
-        logging.warning(e)
-        return {e}
-
-
-def _is_like(user_obj, post_obj):
-    ''' Uploading like object '''
-    try:
-
-        like_op = Like.objects.value_list(user_related_like=user_obj, post_related_like =post_obj)
-        print (like_op)
-        '''              
-        if not isinstance(like, models.Like):
-            raise Exception('Like obj to post per user obj is Not unique')
-        '''
-
-        return False
-
-    except Exception as e:
-        logging.warning(e)
-        return {type(e).__name__, like}
-
-
 # Handle users
 class User(models.Model):
     fullname = models.CharField(max_length=100, default='Name_not_known')
@@ -139,7 +72,6 @@ class User(models.Model):
     @classmethod
     def create(cls, email):
         '''Create new user object in table'''
-
         try:
             fullname = 'Anonymous'
 
@@ -156,35 +88,38 @@ class User(models.Model):
                     logging.warning('Clearbit data addition failed')
                 user = cls(email=email.decode('utf-8'), fullname=fullname)
                 user.save()
-                return (_encode(user.email, user.id))
+                return _encode(user.email, user.id)
 
         except Exception as e:
             logging.warning(e)
             return {e}
 
     def inc_posts(self):
-        ''' dispose if on save works'''
-
+        ''' increment post numerator'''
         try:
             self.number_of_posts += 1
             self.save()
 
             return True
 
-        except Exception:
+        except Exception as e:
             logging.warning(f'User {self.number_of_posts} update error')
             return False
 
     def __str__(self):
-        '''Represent user name as the object'''
         return self.fullname
+
+
+def get_user():
+    return User.objects.get(pk=1)
 
 
 # handle post : Relate many -> one to user
 class Post(models.Model):
     post_text = models.CharField(max_length=150)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default = get_user)
     number_of_likes = models.IntegerField(default=0)
+    #user = models.ManyToManyField("User", through='Like')
 
     @classmethod
     def create(cls, user_obj):
@@ -199,8 +134,7 @@ class Post(models.Model):
             if post.pk is None:
                 raise Exception('Post Can not be created')
 
-            '''  disposal if on post_save works   '''
-            # Update user's posts_numerator
+            '''Update user's posts_numerator'''
             if not post.user.inc_posts():
                 raise MISMATCH_BETWEEN_ENTITIES(
                     f'Post {post.pk} saved but user number_of_posts in related user {user_obj.pk} did not ')
@@ -208,13 +142,12 @@ class Post(models.Model):
             return post.pk
 
         except Exception as e:
-            del post
+            post.delete()
             logging.warning(e)
             return {e, f'Post obj deleted, no mismatches'}
 
-    def update_post(self, diff):
-        ''' dispose if on save works'''
-
+    def update_likes(self, diff):
+        '''update likes numerator for the post'''
         try:
             self.number_of_likes += diff
             self.save()
@@ -225,13 +158,8 @@ class Post(models.Model):
             logging.warning(f'Post {self.number_of_likes} update error')
             return False
 
-
     def __str__(self):
-        ''' Represent post text as the object'''
         return self.post_text
-
-    def __repr__(self):
-        return str(self.post.pk)
 
 
 # handle like: Relate many -> many to user, Relate many -> many to user
@@ -240,52 +168,54 @@ class Like(models.Model):
     user_related_like = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     post_related_like = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
 
-    # Do like
+    '''Do like'''
+
     @classmethod
     def create(cls, user_related_like, post_related_like):
         '''Create new like object in table'''
         try:
-
             like_text = f'Like number {post_related_like.number_of_likes + 1} of {post_related_like.post_text}'
 
             like = cls(user_related_like=user_related_like, post_related_like=post_related_like, like_text=like_text)
             like.save()
 
-            '''  disposal if on post_save works   '''
-            # Update user's posts_numerator
-            if not like.post_related_like.update_post(1):
-
+            '''Update user's posts_numerator'''
+            if not like.post_related_like.update_likes(1):
                 raise MISMATCH_BETWEEN_ENTITIES(
                     f'Like {like.pk} saved but user number_of_posts in related user {post_related_like.pk} did not ')
+
+            return like.pk
+        except MISMATCH_BETWEEN_ENTITIES as e:
+            '''  disposal if on post_save works   '''
+            like.delete()
+            logging.warning(e)
+            return {e, f'like obj deleted, no mismatches'}
+
+        except Exception as e:
+            '''  Did not create object, can be due to: unique constraints or communication   '''
+            logging.warning(e)
+            return {e}
+
+    '''do_unlike'''
+    def do_unlike(self):
+        try:
+            self.delete()
+            '''Update user's posts_numerator'''
+            if not self.post_related_like.update_likes(-1):
+                raise MISMATCH_BETWEEN_ENTITIES(
+                    f'Like {self.pk} saved but user number_of_posts in related user {self.post_related_like.pk} did not ')
 
             return True
 
         except Exception as e:
-            del like
             logging.warning(e)
-            return {e, f'like obj deleted, no mismatches'}
-
-    def retreive_like(self,user_object,post_object):
-        try:
-
-            op_likes = Like.objects.value_list(user_related_like =user_object)
-
-            #, post_related_like =post_object)
-            if not isinstance(op_likes, models.Like):
-                raise Exception('Like obj to post per user obj is Not unique')
-
-            return False
-
-        except Exception as e:
-            logging.warning(e)
-            return {type(e).__name__, op_likes}
-
+            return {type(e).__name__}
 
     def __str__(self):
         ''' Represent post text as the object'''
         return self.like_text
-
-    def __repr__(self):
-        return self.pk
-
+    '''
+    class Meta:
+        unique_together = [("user_related_like", "post_related_like")]
+    '''
 
