7f9ff29fc09b3e1c6007b2795ca91c8a1b56cc97
Social network -Django-Python <sg.sapir@gmail.com>
2020-10-13 08:04:44 +0300

    Update views.py
    func reorganize

--- Server/core/views.py
+++ Server/core/views.py
@@ -2,90 +2,82 @@
 
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
-from django.db.models import Q
-
 from . import models
 
 
 def index(request):
+    '''For uploading the app'''
     return HttpResponse('Hello, You''re at the social network of Trade_core Server.  '
                         'You can go to Admin, by adding: /admin to url, or run BOT with specific action to perform in '
                         'App core')
-# Create new user
+
+
 @csrf_exempt
 def sign_up(request):
+    '''Create new user'''
     jwt_code = models.User.create(request.body)
     return HttpResponse(jwt_code)
 
 
-# Login specific user
 @csrf_exempt
 def login(request):
-    succ_fail_flag = models._login(request.body)
+    '''Login specific user'''
+    response = False
 
-    return HttpResponse(succ_fail_flag)
+    op_mail, op_id = models._decode(request.body)
+    if not op_id == -1:
+        user_obj = models.User.objects.get(pk=op_id)
+
+        if user_obj.pk != None and user_obj.email == op_mail:
+            response = user_obj
+
+    return HttpResponse(response)
 
 
-# After login create some posts
 @csrf_exempt
 def create_post(request):
+    '''Create post'''
 
-    user = models._retreive_user(request.body)
+    user = models.User.objects.get(pk=(request.body).decode('utf-8'))
+
     # influence from segment issue error to resolve
-    if isinstance(user,models.User):
-        response= models.Post.create(user)
+    if isinstance(user, models.User):
+        response = models.Post.create(user)
     else:
         response = user
 
     return HttpResponse(response)
 
 
-# After login create do likes
 @csrf_exempt
 def do_like(request):
+    '''Create Like - do like'''
+    res = False
+
     op_like = (request.body).decode('utf-8').split('=')
     # bring user  & post objects
+    user = models.User.objects.get(pk=op_like[0])
+    post = models.Post.objects.get(pk=op_like[1])
 
-    user = models._retreive_user(op_like[0])
-    if isinstance(user, models.User):
-        post = models._is_post(int(op_like[1]))
-        if isinstance(post, models.Post):
-            succ_fail_flag= models.Like.create(user,post)
-        else:
-            succ_fail_flag = post
-    else:
-        succ_fail_flag = user
+    if isinstance(user, models.User) and isinstance(post, models.Post):
+        res = models.Like.create(user,post)
 
-    return HttpResponse(succ_fail_flag)
+    return HttpResponse(res)
 
-# After login create do unlike
+
 @csrf_exempt
 def do_unlike(request):
+    '''Delete Like - do unlike'''
+    res = False
+
     op_unlike = (request.body).decode('utf-8').split('=')
     # bring user  & post objects
+    user = models.User.objects.get(pk=op_unlike[0])
+    post = models.Post.objects.get(pk=op_unlike[1])
 
-    user = models._retreive_user(op_unlike[0])
-    if isinstance(user, models.User):
-            post = models._is_post(int(op_unlike[1]))
-            if isinstance(post, models.Post):
-                #like = models.Like.retreive_like(user, post)
-                #like = models.Like.objects.value_list(user_related_like=user, post_related_like =post)
-                #like = models._is_like(user,post)
-                op_like = models.Like.objects.filter(user_related_like=user)
-                for i in op_like:
-                    like= op_like.objects.get(post_related_like = post)
-                    print(f'{like}Y')
-                if isinstance(op_like, models.Like):
-                    del op_like
-                else:
-                    succ_fail_flag = op_like
-            else:
-                succ_fail_flag = post
-    else:
-        succ_fail_flag = user
+    if isinstance(user, models.User) and isinstance(post, models.Post):
+        unlike = models.Like.objects.filter(user_related_like=user, post_related_like=post)
+        if isinstance(unlike[0], models.Like):
+            res = unlike[0].do_unlike()
 
-    return HttpResponse(succ_fail_flag)
-
-
-
-
+    return HttpResponse(res)
