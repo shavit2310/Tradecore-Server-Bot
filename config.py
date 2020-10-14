28d1344986b7672ec661b8468cf71f7bc78eb6b6
Social network -Django-Python <sg.sapir@gmail.com>
2020-10-14 11:16:17 +0300

    Update config.py
    Last touch

--- config.py
+++ config.py
@@ -10,26 +10,23 @@ mail_bank_list = ['alex@clearbit.com', 'anat@eby.co.il', 'jobs@twingo.com', 'lib
                   'taya@govreensegal.com']  # deliverable,undeliverable,riski
 
 '''URL settings '''''
-URL ="{APP_URL}:{APP_PORT}".format(APP_URL='http://127.0.0.1',APP_PORT = '8000')
+URL ='{APP_URL}:{APP_PORT}'.format(APP_URL='http://127.0.0.1',APP_PORT='8000')
 
 users = []  # List of actual created users in Server(DB)
 new_line = '\n'
 
 
-class BOT_ERROR_FINISHED(Exception):
-    #print('No users to create')
+class BOT_FINISHED_ERROR(Exception):
     pass
 
 
-class BOT_FINISHED(Exception):
-    #print('No users with 0 likes left or likes to do')
+class BOT_FINISHED_NO_ERROR(Exception):
     pass
 
 
 class CRITICAL_BET_ERROR(Exception):
     '''A new object (post or like) created in the server\DB
     but failed to updated in the BOT'''
-    #print('Mismatch between server and BOT')
     pass
 
 
@@ -37,8 +34,6 @@ class CRITICAL_IN_ERROR(Exception):
     '''In server\DB a new object was created. Failed to update fully in:
     post per user likes numerator failed or
     user likes numerator failed to updated'''
-    #print (f'In server\DB a new object was created. Failed to update fully in BOT')
-
     pass
 
 
